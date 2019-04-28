08307d40d0fb8c794178e16a12eb34c11bc2633e
Eric Lee <Raidenlkl@gmail.com>
2019-04-28 15:19:55 +1000

    Test Case finished

---
+++ Code/Assignment/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Assignment
+{
+    public class Helper
+    {
+        public static T[] ShuffleArray<T>(T[] array)
+        {
+            Random r = new Random();
+            for (int i = array.Length; i > 0; i--)
+            {
+                int j = r.Next(i);
+                T k = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = k;
+            }
+
+            return array;
+        }
+    }
+}
--- Code/Assignment/TestCase.cs
+++ Code/Assignment/TestCase.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using NUnit.Framework;
+
 namespace Assignment
 {
+    [TestFixture]
     public class TestCase
     {
-        public TestCase()
+        public int[] inputArray;
+        [Test()]
+        public void Small_To_Big()
         {
+            inputArray = new int[] { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61 };
+            Assert.That(Algorithm.MinDistance(inputArray) == 1);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 1);
+        }
+
+        [Test()]
+        public void Big_To_Small()
+        {
+            inputArray = new int[] { 61,59,53,47,43,41,37,31,29,23,19,17,13,11,7,5,3,2 };
+            Assert.That(Algorithm.MinDistance(inputArray) == 1);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 1);
+        }
+
+        [Test()]
+        public void Duplicate_Number_In_Set()
+        {
+            inputArray = new int[] { 1, 65535, 65535, 3, 6, 2, 0, 2, 7 }; ;
+            Assert.That(Algorithm.MinDistance(inputArray) == 0);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 0);
+        }
+
+        [Test()]
+        public void Duplicate_Number_At_Begin_And_End()
+        {
+            inputArray = new int[] { 65535, 65535, 3, 6, 2, 0, 2, 7 , 65535}; ;
+            Assert.That(Algorithm.MinDistance(inputArray) == 0);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 0);
+        }
+
+        [Test()]
+        public void RandomPosition()
+        {
+            int[] array = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61 };
+            inputArray = Helper.ShuffleArray(array);
+
+            Assert.That(Algorithm.MinDistance(inputArray) == 1);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 1);
+        }
+
+        [Test()]
+        public void NegativeValues()
+        {
+            inputArray = new int[] { -9, -23, -32, 0  }; ;
+            Assert.That(Algorithm.MinDistance(inputArray) == 9);
+            Assert.That(Algorithm.MinDistance2(inputArray) == 9);
         }
     }
 }
