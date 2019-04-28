be78f4e7f63660b308df80dc70ff34599447de74
Eric Lee <Raidenlkl@gmail.com>
2019-04-28 14:33:39 +1000

    Algorithm done

---
+++ Code/Assignment/Algorithm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Assignment
+{
+    public class Algorithm
+    {
+        public static int MinDistance(int [] A)
+        {
+            //  Input : Array A[0 ... n-1] of numbers
+            //  Output : Minimum distance between two of its elements
+            int dmin = int.MaxValue;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                for(int j = 0; j< A.Length; j++)
+                {
+                    if ((i != j) && ( Math.Abs(A[i] - A[j]) < dmin))
+                    {
+                        Console.WriteLine("Execute");
+                        Console.WriteLine("i : ");
+                        Console.WriteLine(i);
+                        Console.WriteLine("j : ");
+                        Console.WriteLine(j);
+                        dmin = Math.Abs(A[i] - A[j]);
+                    }
+                }
+            }
+
+            return dmin;
+        }
+
+        public static int MinDistance2(int[] A)
+        {
+            //  Input : An Array A[0 ... n-1] of numbers
+            //  Output : The minimum distance d between two of its elements
+            int dmin = int.MaxValue;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    int temp = Math.Abs(A[i] - A[j]);
+
+                    if (temp < dmin)
+                    {
+                        dmin = temp;
+                    }
+                }
+            }
+
+            return dmin;
+        }
+
+    }
+}
---
+++ Code/Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int [] test1 = new int[]{ 0, 1 ,5, 65535, 65535, 65535, 65535, 65535 };
+            int[] test2 = new int[] { 1, 0, 3, 7, 5, 65535, 65535, 65535, 65535 };
+            int[] test3 = new int[] { 5, 3, 0, 65535, 1, 7, 65535, 65535, 65535 };
+            int[] test4 = new int[] { 65535, 7, 65535, 0, 2, 65535, 3, 65535, 65535 };
+
+            int[] test5 = new int[] { 65536, 5, 1, 2, 0, 3, 6, 9, 65535 };
+            int[] test6 = new int[] { 65535, 65535, 65535, 3, 6, 65535 , 0, 2, 7 };
+            int[] test7 = new int[] { 65535, 65535, 65535, 65535, 9, 5, 2, 0, 4};
+            int[] test8 = new int[] { 65535, 65535, 65535, 65535, 65535, 7 ,4 ,8 };
+            //Console.WriteLine( Math.Abs( test1[0] - test1[2] ));
+            Console.WriteLine(Algorithm.MinDistance2(test1));
+            //Console.WriteLine(Algorithm.MinDistance(test2));
+            //Console.WriteLine(Algorithm.MinDistance(test3));
+            //Console.WriteLine(Algorithm.MinDistance(test4));
+            //Console.WriteLine(Algorithm.MinDistance(test5));
+            //Console.WriteLine(Algorithm.MinDistance(test6));
+            //Console.WriteLine(Algorithm.MinDistance(test7));
+            //Console.WriteLine(Algorithm.MinDistance(test8));
+        }
+    }
+}
